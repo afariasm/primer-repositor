7c9d8aa7061ef765640b09db2571a0afe04a5e75
alfariasm <116596452+afariasm@users.noreply.github.com>
2022-12-01 09:48:59 -0300

    Update Classbanco.py

--- Tarea banco/Classbanco.py
+++ Tarea banco/Classbanco.py
@@ -1,37 +1,37 @@
 class Bank:
     #constructor
     def __init__(self,id,name,surname,bank,key,full_money = 0):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.bank = bank
-        self.key = key
-        self.full_money = 0
+        self._id = id
+        self._name = name
+        self._surname = surname
+        self._bank = bank
+        self._key = key
+        self._full_money = 0
     
     #metodo mostrar en pantalla
     def print_user(self):
         print("--------------------------")
         print("---DATOS DEL USUARIO---")
-        print("nombre : ",self.name)
-        print ("su RUT es : ",self.id)
-        print("Su banco es:",self.bank)
-        print("Su dinero actual es:",self.full_money)
+        print("nombre : ",self._name)
+        print ("su RUT es : ",self._id)
+        print("Su banco es:",self._bank)
+        print("Su dinero actual es:",self._full_money)
         
     #metodo de deposito
     def deposit(self):
         id = 0
-        while(self.id != id): #while en caso de que el rut no sea el mismo 
+        while(self._id != id): #while en caso de que el rut no sea el mismo 
             id = int(input("ingrese su RUT : ")) 
-            if(self.id == id):
+            if(self._id == id):
                 break
-            elif(self.id != id):
+            elif(self._id != id):
                 print("el RUT ingresado no es valido, intente nuevamente")
         key = ""
-        while (self.key != key): #while en caso de que la clave sea incorrecta 
+        while (self._key != key): #while en caso de que la clave sea incorrecta 
             key = input("Ingrese su clave :")
-            if(self.key == key):
+            if(self._key == key):
                 break
-            elif(self.key != key):
+            elif(self._key != key):
                 print("La clave ingresada es incorrecta, intente nuevamente")
         deposit_amount = 0
         while(deposit_amount <= 0  ): #
@@ -39,4 +39,24 @@ class Bank:
             if (0 <deposit_amount ):
                 print("la cantidad a depostiar es : ",deposit_amount)
                 break
-        self.full_money = self.full_money + deposit_amount 
+        self._full_money = self._full_money + deposit_amount 
+    def get_id(self):
+        return self._id
+
+    def get_name(self):
+        return self._name
+
+    def get_surname(self):
+        return self._surname
+
+    def get_bank(self):
+        return self._bank
+
+    def get_key(self):
+        return self._key
+
+    def get_full_money(self):
+        return self._full_money
+
+    
+    
